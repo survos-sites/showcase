4100012212bbc4a3fc284028fe1730e2cf556af9
Tac Tacelosky <tacman@gmail.com>
2024-03-29 07:03:27 -0400

    tests for make:controller

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,17 +3,32 @@
 namespace App\Controller;
 
 use App\Repository\ProjectRepository;
+use Survos\Bundle\MakerBundle\Service\GeneratorService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Zenstruck\Console\RunsCommands;
+use Zenstruck\Console\RunsProcesses;
 
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
     public function index(ProjectRepository $projectRepository): Response
     {
-        return $this->render('app/index.html.twig', [
+        return $this->render('home.html.twig', [
             'projects' => $projectRepository->findAll()
+        ]);
+    }
+
+    #[Route('/generate', name: 'app_generate')]
+    public function generate(GeneratorService $generatorService): Response
+    {
+        $ns = $generatorService->generateController($nsName = "TestSomething");
+        $class = $ns->getClasses()[array_key_first($ns->getClasses())];
+        $generatorService->addMethod($class, 'mymethod');
+//        dd($ns, $class, '<?php ' . $ns);
+        return $this->render('controller.html.twig', [
+            'ns' => $ns
         ]);
     }
 }
