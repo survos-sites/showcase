334b116b1654a1405639cb96a7ebf504cc54758e
Tac Tacelosky <tacman@gmail.com>
2025-11-16 13:54:53 -0500

    update to Symfony 7.4-RC2

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+import './stimulus_bootstrap.js';
 import 'instantsearch.css/themes/algolia.min.css';
 /*
  * Welcome to your app's main JavaScript file!
@@ -8,5 +9,4 @@ import 'instantsearch.css/themes/algolia.min.css';
 import './styles/app.css';
 import '@tabler/core';
 import '@tabler/core/dist/css/tabler.min.css';
-import './bootstrap.js';
 import 'asciinema-player/dist/bundle/asciinema-player.css'
